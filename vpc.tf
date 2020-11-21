80d2ce5c3c279e6c309ecf3530f187eae7d1f14b
root <root@ip-172-31-46-196.us-east-2.compute.internal>
2020-11-21 06:24:29 +0000

    added all conf

---
+++ vpc.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "terraform-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "terraform-vpc"
+  }
+}
+
+output "vpcid"{
+  value = "${aws_vpc.terraform-vpc.id}"
+}
